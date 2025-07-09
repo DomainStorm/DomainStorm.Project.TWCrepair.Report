b012bbffc596bbabebca29e3debf5e02d3abc0ee
why <whyang0926@gmail.com>
2025-07-09 22:56:22 +0800

    DA007 當日流量曲線圖

---
+++ DomainStorm.Project.TWCrepair.Report.Web/Services/Impl/Mock/DA007Service.cs
@@ -0,0 +1,55 @@
+﻿using DomainStorm.Framework.Services;
+using static DomainStorm.Project.TWCrepair.Report.Web.ReportCommandModel.DA007.V1;
+using DomainStorm.Project.TWCrepair.Report.Web.Views.Dashboards;
+
+namespace DomainStorm.Project.TWCrepair.Report.Web.Services.Impl.Mock
+{
+    /// <summary>
+    /// 當日流量曲線圖
+    /// </summary>
+    public class DA007Service : IGetService<DA007, string>
+    {
+        public Task<DA007> GetAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DA007> GetAsync<TQuery>(IQuery condition) where TQuery : IQuery
+        {
+            return condition switch
+            {
+                QueryDA007 e => Query(e),
+                _ => throw new ArgumentOutOfRangeException(nameof(condition), condition, null)
+            };
+        }
+
+        public async Task<DA007> Query(QueryDA007 condition)
+        {
+
+            var result = new DA007();
+            var today = DateTime.Today;
+            for(int i = 0; i< 1440; i++)
+            {
+                result.PlotlyJson.Data.First().X.Add(today.ToString("HH:mm"));
+                result.PlotlyJson.Data.First().Y.Add( ((int)(i / 10)).ToString());
+                today = today.AddMinutes(1);
+            }
+            return result;
+        }
+
+        public Task<DA007[]> GetListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DA007[]> GetListAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DA007[]> GetListAsync<TQuery>(IQuery condition) where TQuery : IQuery
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DomainStorm.Project.TWCrepair.Report.Web/Views/Dashboards/DA007.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using DomainStorm.Project.TWCrepair.Shared.ViewModel;
+using static DomainStorm.Project.TWCrepair.Repository.CommandModel.Report.V1;
+
+namespace DomainStorm.Project.TWCrepair.Report.Web.Views.Dashboards;
+
+public class DA007 : ReportDataModel
+{
+    public PlotlyJson PlotlyJson { get; set; } = new()
+    {
+        Data = new List<Datum>
+            {
+                new()
+                {
+                    Type = "line",
+                    X = new List<string>(),
+                    Y= new List<string>()
+                }
+            },
+        Layout = new Layout
+        {
+            Title = "當日流量曲線圖",
+            Autosize = true,
+            Margin = new Margin(),
+            Xaxis = new Axis(),
+            Yaxis = new Axis
+            {
+                Title = "流量(cmd)"
+            }
+        }
+    };
+}
+
+
