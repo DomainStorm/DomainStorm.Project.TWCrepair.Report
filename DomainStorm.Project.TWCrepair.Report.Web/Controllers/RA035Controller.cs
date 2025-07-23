924528a297ad15a04f2fa428a1f5ea64dbdbfc5a
why <whyang0926@gmail.com>
2025-07-23 15:33:39 +0800

    RA035 年度計畫報告-附表五、系統檢修漏作業流程圖

---
+++ DomainStorm.Project.TWCrepair.Report.Web/Controllers/RA035Controller.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using DomainStorm.Framework.Services;
+using static DomainStorm.Project.TWCrepair.Report.Web.ReportCommandModel.RA035.V1;
+using static DomainStorm.Project.TWCrepair.Repository.CommandModel.Report.V1;
+using System.Net.Mime;
+using DomainStorm.Project.TWCrepair.Report.Web.Views;
+
+namespace DomainStorm.Project.TWCrepair.Report.Web.Controllers;
+
+/// <summary>
+/// 年度計畫報告-附表五、系統檢修漏作業流程圖
+/// </summary>
+[ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+[Route("api/ra035")]
+public class RA035Controller : ControllerBase
+{
+    private readonly IGetService<Stream, ReportConvertRequest> _reportService;
+
+    public RA035Controller(
+        IGetService<DateTime, Guid> ra035DateService,
+        IGetService<Stream, ReportConvertRequest> reportService)
+    {
+        _reportService = reportService;
+    }
+
+   
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] QueryRA035 request)
+    {
+        var ra035Model = new RA035();
+        var convertRequest = new ReportConvertRequest
+        {
+            ViewName = "/Views/RA035.cshtml",
+            Model = ra035Model,
+            Extension = request.Extension
+        };
+        var outStream = await _reportService.GetAsync(convertRequest);
+        var outFileName = $"{System.IO.Path.GetFileNameWithoutExtension(convertRequest.ViewName)}.{convertRequest.Extension.ToString().ToLower()}";
+        return File(outStream, MediaTypeNames.Application.Octet, outFileName);
+
+    }
+}
