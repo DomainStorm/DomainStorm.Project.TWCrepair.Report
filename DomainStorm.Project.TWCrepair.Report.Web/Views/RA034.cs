f663b5368297c3a7e48ef633161a3c7fed572e78
why <whyang0926@gmail.com>
2025-08-12 20:50:55 +0800

    RA036 度計畫報告-附表六、各系統管徑、管長暨附屬設備統計表

---
+++ DomainStorm.Project.TWCrepair.Report.Web/Views/RA034.cs
@@ -0,0 +1,35 @@
+﻿using DomainStorm.Project.TWCrepair.Shared.ViewModel;
+using static DomainStorm.Project.TWCrepair.Repository.CommandModel.Report.V1;
+
+namespace DomainStorm.Project.TWCrepair.Report.Web.Views;
+
+/// <summary>
+/// 年度計畫報告-附表四、檢漏作業計劃差旅費分析表
+/// </summary>
+public class RA034 : ReportDataModel
+{
+    public int Year { get; set; }
+
+    /// <summary>
+    /// 現有人員數-師
+    /// </summary>
+    public int? CurrentPeople1 { get; set; }
+
+    /// <summary>
+    /// 現有人員數-員
+    /// </summary>
+    public int? CurrentPeople2 { get; set; }
+
+    /// <summary>
+    /// 現有人員數-士
+    /// </summary>
+    public int? CurrentPeople3 { get; set; }
+
+
+}
+
+
+
+
+
+
