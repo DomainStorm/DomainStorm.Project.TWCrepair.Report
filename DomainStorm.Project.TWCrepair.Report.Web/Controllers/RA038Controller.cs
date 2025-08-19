0158a02e36b2e55275f058a3e917cd0916bd74ef
why <whyang0926@gmail.com>
2025-08-19 17:28:38 +0800

    RA038 空白頁

---
+++ DomainStorm.Project.TWCrepair.Report.Web/Controllers/RA038Controller.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using DomainStorm.Framework.Services;
+using static DomainStorm.Project.TWCrepair.Repository.CommandModel.Report.V1;
+using System.Net.Mime;
+using DomainStorm.Project.TWCrepair.Report.Web.Views;
+using static DomainStorm.Project.TWCrepair.Report.Web.ReportCommandModel.RA038.V1;
+
+
+namespace DomainStorm.Project.TWCrepair.Report.Web.Controllers;
+
+/// <summary>
+/// 空白頁
+/// </summary>
+[ApiController]
+[Route("api/ra038")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+public class RA038Controller : ControllerBase
+{
+    private readonly IGetService<Stream, ReportConvertRequest> _reportService;
+
+    public RA038Controller(
+        IGetService<Stream, ReportConvertRequest> reportService
+        )
+    {
+        _reportService = reportService;
+    }
+
+
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] QueryRA038 request)
+    {
+        var RA038Model = new RA038
+        {
+            SheetName = request.SheetName,
+        };
+        var convertRequest = new ReportConvertRequest
+        {
+            ViewName = "/Views/RA038.cshtml",
+            Model = RA038Model,
+            Extension = request.Extension
+        };
+        var outStream = await _reportService.GetAsync(convertRequest);
+        var outFileName = $"{System.IO.Path.GetFileNameWithoutExtension(convertRequest.ViewName)}.{convertRequest.Extension.ToString().ToLower()}";
+        return File(outStream, MediaTypeNames.Application.Octet, outFileName);
+    }
+}
---
+++ DomainStorm.Project.TWCrepair.Report.Web/Views/RA038.cs
@@ -0,0 +1,14 @@
+﻿using static DomainStorm.Project.TWCrepair.Repository.CommandModel.Report.V1;
+
+namespace DomainStorm.Project.TWCrepair.Report.Web.Views;
+
+/// <summary>
+/// 空白頁
+/// </summary>
+public class RA038 : ReportDataModel
+{
+    /// <summary>
+    /// 工作表名稱
+    /// </summary>
+    public string SheetName { get; set; }
+}
