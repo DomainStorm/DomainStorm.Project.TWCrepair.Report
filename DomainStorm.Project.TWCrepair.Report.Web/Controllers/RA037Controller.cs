4851a407cf95f7701af387b61824c29a07e9e54b
why <whyang0926@gmail.com>
2025-09-10 15:47:45 +0800

    RA037 度計畫報告-附表七、各系統大區NRW

---
+++ DomainStorm.Project.TWCrepair.Report.Web/Controllers/RA037Controller.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using DomainStorm.Framework.Services;
+using static DomainStorm.Project.TWCrepair.Repository.CommandModel.Report.V1;
+using System.Net.Mime;
+using DomainStorm.Project.TWCrepair.Report.Web.Views;
+using static DomainStorm.Project.TWCrepair.Report.Web.ReportCommandModel.RA037.V1;
+
+
+namespace DomainStorm.Project.TWCrepair.Report.Web.Controllers;
+
+/// <summary>
+/// 年度計畫報告-附表七、各系統大區NRW
+/// </summary>
+[ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+[Route("api/ra037")]
+public class RA037Controller : ControllerBase
+{
+    private readonly IGetService<RA037, string> _RA037Service;
+    private readonly IGetService<Stream, ReportConvertRequest> _reportService;
+
+    public RA037Controller(
+        IGetService<RA037, string> RA037Service,
+        IGetService<Stream, ReportConvertRequest> reportService
+        )
+    {
+        _RA037Service = RA037Service;
+        _reportService = reportService;
+    }
+
+   
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] QueryRA037 request)
+    {
+        var RA037Model = await _RA037Service.GetAsync<QueryRA037>(request);
+        var convertRequest = new ReportConvertRequest
+        {
+            ViewName = "/Views/RA037.cshtml",
+            Model = RA037Model,
+            Extension = request.Extension
+        };
+        var outStream = await _reportService.GetAsync(convertRequest);
+        var outFileName = $"{System.IO.Path.GetFileNameWithoutExtension(convertRequest.ViewName)}.{convertRequest.Extension.ToString().ToLower()}";
+        return File(outStream, MediaTypeNames.Application.Octet, outFileName);
+    }
+}
